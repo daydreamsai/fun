2965cf465572e8495352215ae14b2757d4740576
Galego <bmgalego@gmail.com>
2025-06-06 14:43:32 +0000

    fix envs

--- client/build.sh
+++ client/build.sh
@@ -18,9 +18,10 @@ done
 
 echo "API_URL: ${API_URL}"
 echo "RELEASE: ${RELEASE}"
+echo "NODE_URL: ${NODE_URL}"
 
 # echo ${API_URL}
 
-docker build --platform linux/amd64 --build-arg API_URL="${API_URL}" --build-arg NODE_URL="${NODE_URL}" --build-arg CHAIN_ID="${CHAIN_ID}" -t dreams-play-client:${RELEASE} .
+docker build --platform linux/amd64 --build-arg API_URL=${API_URL} --build-arg NODE_URL=${NODE_URL} --build-arg CHAIN_ID=${CHAIN_ID} -t dreams-play-client:${RELEASE} .
 docker tag dreams-play-client:${RELEASE} europe-southwest1-docker.pkg.dev/daydreams-cloud/dreams-fun/play-client:${RELEASE}
 docker push europe-southwest1-docker.pkg.dev/daydreams-cloud/dreams-fun/play-client:${RELEASE}
